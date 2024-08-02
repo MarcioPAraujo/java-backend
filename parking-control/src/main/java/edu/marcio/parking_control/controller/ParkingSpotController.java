9dc96c2d179260e45950310015e278743671e2f5
MarcioPAraujo <marciopereiraaraujo1997d1@gmail.com>
2024-08-02 11:52:47 -0300

    update mathod was added

--- parking-control/src/main/java/edu/marcio/parking_control/controller/ParkingSpotController.java
+++ parking-control/src/main/java/edu/marcio/parking_control/controller/ParkingSpotController.java
@@ -6,8 +6,13 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -18,13 +23,15 @@ import org.springframework.web.bind.annotation.RestController;
 import edu.marcio.parking_control.dto.ParkingSpotDto;
 import edu.marcio.parking_control.model.ParkingSpotModel;
 import edu.marcio.parking_control.service.ParkingSpotService;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -63,8 +70,14 @@ public class ParkingSpotController {
     
     }
     @GetMapping
-    public ResponseEntity<List<ParkingSpotModel>> getAllParkingSpots() {
-        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll());
+    public ResponseEntity<Page<ParkingSpotModel>> getAllParkingSpots(@PageableDefault(
+        page = 0,/*initial page */
+         size = 5,/*items per page */
+          sort = "id",/*attribute used to order the items on the page */
+           direction = Sort.Direction.ASC)/*how the items will be sorted ascendent or descendent */
+           Pageable pageable) {
+
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll(pageable));
     }
     
     @GetMapping("/{id}")
@@ -89,6 +102,24 @@ public class ParkingSpotController {
         return ResponseEntity.status(HttpStatus.OK).body("Mr."+parkingSpotOptional.get().getResponsibleName()+" you're no longer part of the coninental");
 
 
+    }
+
+    @Transactional
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateParkingSpot(@PathVariable (value = "id") UUID id, @RequestBody @Valid ParkingSpotDto parkingSpotDto) {
+        Optional<ParkingSpotModel> parkingOptional = parkingSpotService.findById(id);
+        if (parkingOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("this parking doesn't exist");
+        }
+        ParkingSpotModel parkingSpotModel = new ParkingSpotModel();
+        /*copying the object recived by URI to the a new instance, this way is efficint that setting each one of the attributes manually */
+        BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
+        /*setting datas that are constants, id and record date */
+        parkingSpotModel.setId(parkingOptional.get().getId());
+        parkingSpotModel.setRegistrationDate(parkingOptional.get().getRegistrationDate());
+
+        // saving the updates
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.save(parkingSpotModel));
     }
     
 
